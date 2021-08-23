50d46ffe92f4346de74168ec241d8c9aa5c717a5
Urban Andrius <urban.andrius@gmail.com>
2021-08-23 20:35:25 +0300

    Moved episode list view to fragment

--- app/src/main/java/com/anurbanv/tvshowcase/MainActivity.kt
+++ app/src/main/java/com/anurbanv/tvshowcase/MainActivity.kt
@@ -3,50 +3,30 @@ package com.anurbanv.tvshowcase
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.anurbanv.tvshowcase.adapter.EpisodeAdapter
+import androidx.fragment.app.commit
 import com.anurbanv.tvshowcase.databinding.ActivityMainBinding
-import com.anurbanv.tvshowcase.entity.Episode
-import com.anurbanv.tvshowcase.util.JsonUtil
-import com.google.gson.Gson
-import com.google.gson.JsonObject
+import com.anurbanv.tvshowcase.fragment.EpisodeListFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private val gson = Gson()
+    private lateinit var episodeListFragment: EpisodeListFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
-        val adapter = EpisodeAdapter()
-
-        binding.rvEpisodes.layoutManager = LinearLayoutManager(this)
-        binding.rvEpisodes.adapter = adapter
-
-        val episodeList = parseEpisodesJson()
-        adapter.episodeList = episodeList
-    }
-
-    private fun parseEpisodesJson(): List<Episode> {
-        val episodeList = arrayListOf<Episode>()
-
-        val jsonString = JsonUtil.loadJsonFromAssets(this, "hbo-silicon-valley.json")
-        val fromJson = gson.fromJson(jsonString, JsonObject::class.java)
-
-        for (seasonJson in fromJson.get("seasons").asJsonArray) {
-            for (episodeJson in seasonJson.asJsonObject.get("episodes").asJsonArray) {
-                val season = episodeJson.asJsonObject.get("Season").asInt
-                val episodeNr = episodeJson.asJsonObject.get("Episode").asInt
-                val name = episodeJson.asJsonObject.get("Title").asString
-                val imageUrl = episodeJson.asJsonObject.get("Poster").asString
-                val summary = episodeJson.asJsonObject.get("Plot").asString
-
-                episodeList.add(Episode(season, episodeNr, name, imageUrl, summary))
-            }
+        if (savedInstanceState == null) {
+            episodeListFragment = EpisodeListFragment()
         }
 
-        return episodeList
+        showEpisodeListFragment()
+    }
+
+    private fun showEpisodeListFragment() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.fragmentContainerView, episodeListFragment)
+        }
     }
 }
